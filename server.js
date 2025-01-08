5b5f512cc2fabf7e3ee1816da8a2105484ffd47c
lewieville <lewievilleyt@gmail.com>
2025-01-07 23:29:29 -0500

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,18 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const app = express();
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+// Load environment variables
+dotenv.config();
+
+// MongoDB connection
+const mongoURI = process.env.MONGO_URI;
+mongoose
+  .connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected!'))
+  .catch((err) => console.error('MongoDB connection error:', err));
 
 app.use(express.json());
 app.use(cors());
